8bd4052475156ce1e0f2195d768547cbab889bad
ledelmecon <leandro.delgado@outlook.com>
2020-11-02 01:58:13 -0300

    Add files via upload

---
+++ TP3/Clases Instanciables/Alumno.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesAbstractas;
+
+namespace Clases_Instanciables
+{
+    public sealed class Alumno : Universitario
+	{
+		#region Enumerados
+		/// <summary>
+		/// Enumerado que contiene los tres tipos de estado de cuenta.
+		/// </summary>
+		public enum EEstadoCuenta
+		{
+			AlDia,
+			Deudor,
+			Becado
+		}
+		#endregion
+
+		#region Atributos
+		private Universidad.EClases claseQueToma;
+		private EEstadoCuenta estadoCuenta;
+		#endregion
+
+		#region Constructores
+		/// <summary>
+		/// Constructor por defecto de Alumno.
+		/// </summary>
+		public Alumno()
+		{
+		}
+
+		/// <summary>
+		/// Constructor que llama la base e inicializa el atributo:
+		/// - ClaseQueToma.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="nombre"></param>
+		/// <param name="apellido"></param>
+		/// <param name="dni"></param>
+		/// <param name="nacionalidad"></param>
+		/// <param name="claseQueToma"></param>
+		public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Universidad.EClases claseQueToma)
+			: base(id, nombre, apellido, dni, nacionalidad)
+		{
+			this.claseQueToma = claseQueToma;
+		}
+
+		/// <summary>
+		/// Constructor que reutiliza el constructor anterior e inicializa
+		/// el atributo: 
+		/// - estadoCuenta.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="nombre"></param>
+		/// <param name="apellido"></param>
+		/// <param name="dni"></param>
+		/// <param name="nacionalidad"></param>
+		/// <param name="claseQueToma"></param>
+		/// <param name="estadoCuenta"></param>
+		public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Universidad.EClases claseQueToma, EEstadoCuenta estadoCuenta)
+			: this(id, nombre, apellido, dni, nacionalidad, claseQueToma)
+		{
+			this.estadoCuenta = estadoCuenta;
+		}
+		#endregion
+
+		#region Operaciones
+		/// <summary>
+		/// Sobrecarga del operador "==" que compara si un 
+		/// Alumno cumple los requisitos para tomar esa clase.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="clase"></param>
+		/// <returns></returns>
+		public static bool operator ==(Alumno a, Universidad.EClases clase)
+		{
+			bool cumpleRequisitos = false;
+
+			if(a.claseQueToma == clase && a.estadoCuenta != EEstadoCuenta.Deudor)
+				cumpleRequisitos = true;
+
+			return cumpleRequisitos;
+		}
+
+		/// <summary>
+		/// Sobrecarga del operador "!=" que compara si un
+		/// Alumno no toma esa clase
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="clase"></param>
+		/// <returns></returns>
+		public static bool operator !=(Alumno a, Universidad.EClases clase)
+		{
+			return a.claseQueToma == clase;
+		}
+		#endregion
+
+		#region Override
+		/// <summary>
+		/// Método que expone los datos del Alumno.
+		/// </summary>
+		/// <returns>Los datos del Alumno.</returns>
+		protected override string MostrarDatos()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.AppendFormat(base.MostrarDatos());
+
+			if(this.estadoCuenta is EEstadoCuenta.AlDia)
+				sb.AppendFormat("ESTADO DE CUENTA: Cuota al día\n");//tal cuál el tp
+			else
+				sb.AppendFormat($"ESTADO DE CUENTA: {this.estadoCuenta}\n");
+
+			sb.AppendFormat($"{this.ParticiparEnClase()}");
+
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Implementación del método abstracto de la clase Universitario.
+		/// </summary>
+		/// <returns>Las clases que toma el Alumno.</returns>
+		protected override string ParticiparEnClase()
+		{
+			return $"TOMA CLASES DE {this.claseQueToma}.\n";
+		}
+
+		/// <summary>
+		/// Sobrescritura del método ToString() que llama al
+		/// método MostrarDatos().
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return this.MostrarDatos();
+		}
+		#endregion
+
+	}
+}
---
+++ TP3/Clases Instanciables/Jornada.cs
@@ -0,0 +1,192 @@
+﻿using Archivos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Instanciables
+{
+    public class Jornada
+    {
+        #region Atributos
+        private List<Alumno> listaAlumnos;
+        private Universidad.EClases clase;
+        private Profesor instructor;
+        #endregion
+
+        #region Properties
+        /// <summary>
+		/// Propiedad de lectura y escritura,
+		/// que retorna la lista de alumnos.
+		/// </summary>
+        public List<Alumno> Alumnos
+        {
+            get
+            {
+                return this.listaAlumnos;
+            }
+            set
+            {
+                this.listaAlumnos = value;
+            }
+        }
+
+        /// <summary>
+		/// Propiedad de lectura y escritura,
+		/// que retorna la clase.
+		/// </summary>
+        public Universidad.EClases Clase
+        {
+            get
+            {
+                return this.clase;
+            }
+            set
+            {
+                this.clase = value;
+            }
+        }
+
+        /// <summary>
+		/// Propiedad de lectura y escritura,
+		/// que retorna la lista de instructores.
+		/// </summary>
+        public Profesor Instructor
+        {
+            get
+            {
+                return this.instructor;
+            }
+            set
+            {
+                this.instructor = value;
+            }
+        }
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor por defecto que inicializa la lista
+        /// de alumnos.
+        /// </summary>
+        private Jornada()
+        {
+            this.Alumnos = new List<Alumno>();
+        }
+
+        /// <summary>
+		/// Constructor que reutiliza el constructor anterior e inicializa
+        /// los atributos:
+        /// - Clase.
+        /// - Instructor.
+        /// </summary>
+        /// <param name="clase"></param>
+        /// <param name="instructor"></param>
+        public Jornada(Universidad.EClases clase, Profesor instructor) : this()
+        {
+            this.Clase = clase;
+            this.Instructor = instructor;
+        }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Método estático que guardará en un archivo de texto
+        /// los datos de la Jornada.
+        /// </summary>
+        /// <param name="jornada"></param>
+        /// <returns>Si pudo guardar o no.</returns>
+        public static bool Guardar(Jornada jornada)
+        {
+            Texto archivoTxt = new Texto();
+            string ruta = Directory.GetCurrentDirectory() + @"\Jornada.txt";
+            bool pudoGuardar = false;
+
+            if (archivoTxt.Guardar(ruta, jornada.ToString()))
+                pudoGuardar = true;
+
+            return pudoGuardar;
+        }
+
+        /// <summary>
+        /// Método estático que leera el archivo de texto de Jornada.
+        /// </summary>
+        /// <returns>Los datos de la Jornada en formato de texto.</returns>
+        public static string Leer()
+        {
+            Texto archivoTxt = new Texto();
+            string ruta = Directory.GetCurrentDirectory() + @"\Jornada.txt";
+            archivoTxt.Leer(ruta, out string jornadaDatos);
+            return jornadaDatos;
+        }
+        #endregion
+
+        #region Operaciones
+        /// <summary>
+        /// Sobrecarga del operador "==" que compara si el Alumno
+        /// participa en esa clase.
+        /// </summary>
+        /// <param name="j"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static bool operator ==(Jornada j, Alumno a)
+        {
+            bool participaEnLaClase = false;
+            foreach (Alumno item in j.listaAlumnos)
+            {
+                if (item == a)
+                    participaEnLaClase = true;
+            }
+            return participaEnLaClase;
+        }
+
+        /// <summary>
+        /// Sobrecarga del operador "!=" que compara si el Alumno
+        /// no participa en esa clase.
+        /// </summary>
+        /// <param name="j"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static bool operator !=(Jornada j, Alumno a)
+        {
+            return !(j == a);
+        }
+
+        /// <summary>
+        /// Sobrecarga del operador "+" que agrega a un Alumno a la lista
+        /// solamente si no se encuentra cargado.
+        /// </summary>
+        /// <param name="j"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static Jornada operator +(Jornada j, Alumno a)
+        {
+            if (j != a)
+                j.listaAlumnos.Add(a);
+            return j;
+        }
+        #endregion
+
+        #region Override
+        /// <summary>
+		/// Sobrescritura del método ToString() que expone
+		/// los datos de la Jornada.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"CLASE DE {this.Clase.ToString()} POR {this.Instructor.ToString()}");
+            sb.AppendFormat("\nALUMNOS:\n");
+            foreach (Alumno item in this.listaAlumnos)
+            {
+                sb.AppendFormat(item.ToString());
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+    }
+}
