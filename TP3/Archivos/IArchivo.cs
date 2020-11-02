d4999cf3dc9915dcc416d277a7195afb8fbd5a74
ledelmecon <leandro.delgado@outlook.com>
2020-11-02 02:07:39 -0300

    Add files via upload

---
+++ TP3/Archivos/IArchivo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    interface IArchivo<T>
+    {
+        #region Métodos
+        /// <summary>
+        /// Método sin implementación que tendrá como propósito
+        /// guardar datos.
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        bool Guardar(string archivo, T datos);
+
+        /// <summary>
+        /// Método sin implementación que tendrá como propósito
+        /// leer datos.
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        bool Leer(string archivo, out T datos);
+
+        #endregion
+    }
+}
