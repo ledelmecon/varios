5cf301fec2265bfe36773792a11440d60c7e0295
ledelmecon <leandro.delgado@outlook.com>
2020-11-02 02:04:27 -0300

    Add files via upload

---
+++ TP3/Clases Abstractas/Persona.cs
@@ -0,0 +1,249 @@
+﻿using Excepciones;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EntidadesAbstractas
+{
+    public abstract class Persona
+	{
+		#region Enumerados
+		/// <summary>
+		/// Enumerado que contiene los dos tipos de nacionalidad.
+		/// </summary>
+		public enum ENacionalidad
+		{
+			Argentino,
+			Extranjero
+		}
+		#endregion
+
+		#region Atributos
+		private string nombre;
+		private string apellido;
+		private ENacionalidad nacionalidad;
+		private int dni;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Propiedad de lectura y escritura 
+		/// que retorna el nombre de la Persona.
+		/// </summary>
+		public string Nombre 
+		{
+			get
+			{
+				return this.nombre;
+			}
+			set
+			{
+				this.nombre = this.ValidarNombreApellido(value);
+			}
+		}
+
+		/// <summary>
+		/// Propiedad de lectura y escritura 
+		/// que retorna el apellido de la Persona.
+		/// </summary>
+		public string Apellido
+		{
+			get
+			{
+				return this.apellido;
+			}
+			set
+			{
+				this.apellido = this.ValidarNombreApellido(value);
+			}
+		}
+
+		/// <summary>
+		/// Propiedad de lectura y escritura,
+		/// que retorna la nacionalidad de la Persona.
+		/// </summary>
+		public ENacionalidad Nacionalidad
+		{
+			get
+			{
+				return this.nacionalidad;
+			}
+			set
+			{
+				this.nacionalidad = value;
+			}
+		}
+
+		/// <summary>
+		/// Propiedad de lectura y escritura (con validación, primer sobrecarga del método
+		/// ValidarDni()), que retorna el dni de la Persona.
+		/// </summary>
+		public int DNI
+		{
+			get
+			{
+				return this.dni;
+			}
+			set
+			{
+				this.dni = this.ValidarDni(this.Nacionalidad, value);
+			}
+		}
+
+		/// <summary>
+		/// Propiedad de lectura y escritura (con validación, segunda sobrecarga del método
+		/// ValidarDni()), que retorna el dni de la Persona.
+		/// </summary>
+		public string StringToDNI
+		{
+			set
+			{
+				this.dni = this.ValidarDni(this.Nacionalidad, value);
+			}
+		}
+		#endregion
+
+		#region Constructores
+		/// <summary>
+		/// Constructor por defecto de Persona.
+		/// </summary>
+		public Persona()
+		{
+		}
+
+		/// <summary>
+		/// Constructor que inicializa los atributos:
+		/// - nombre.
+		/// - apellido.
+		/// - nacionalidad.
+		/// </summary>
+		/// <param name="nombre"></param>
+		/// <param name="apellido"></param>
+		/// <param name="nacionalidad"></param>
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+		{
+			this.Nombre = nombre;
+			this.Apellido = apellido;
+			this.Nacionalidad = nacionalidad;
+        }
+
+		/// <summary>
+		/// Constructor que reutiliza el constructor anterior e inicializa
+		/// el atributo: 
+		/// - dni (en formato int).
+		/// </summary>
+		/// <param name="nombre"></param>
+		/// <param name="apellido"></param>
+		/// <param name="dni"></param>
+		/// <param name="nacionalidad"></param>
+		public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+			:this(nombre,apellido,nacionalidad)
+		{
+			this.DNI = dni;
+		}
+
+		/// <summary>
+		/// Constructor que reutiliza el constructor anterior e inicializa
+		/// el atributo: 
+		/// - dni (en formato string).
+		/// </summary>
+		/// <param name="nombre"></param>
+		/// <param name="apellido"></param>
+		/// <param name="dni"></param>
+		/// <param name="nacionalidad"></param>
+		public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+			: this(nombre, apellido, nacionalidad)
+		{
+			this.StringToDNI = dni;
+		}
+		#endregion
+
+		#region Métodos
+		/// <summary>
+		/// Método que valida la nacionalidad y el rango que puede tener el dni.
+		/// </summary>
+		/// <param name="nacionalidad"></param>
+		/// <param name="dato"></param>
+		/// <returns></returns>
+		private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+			int retornoDni = default(int);
+
+			switch (nacionalidad)
+			{
+				case ENacionalidad.Argentino:
+					if(dato >= 1 && dato <= 89999999)
+						retornoDni = dato;
+					else
+						throw new NacionalidadInvalidaException("La nacionalidad no se condice con el número de DNI.");
+					break;
+
+				case ENacionalidad.Extranjero:
+					if (dato >= 90000000 && dato < 99999999)
+						retornoDni = dato;
+					else
+						throw new NacionalidadInvalidaException("La nacionalidad no se condice con el número de DNI.");
+					break;
+			}
+
+			return retornoDni;
+		}
+
+		/// <summary>
+		/// Método que valida que el dni sea solo de números y llama a la 
+		/// sobrecargar anterior.
+		/// Caso contrario, lanza la excepción DniInvalidoException.
+		/// </summary>
+		/// <param name="nacionalidad"></param>
+		/// <param name="dato"></param>
+		/// <returns></returns>
+		private int ValidarDni(ENacionalidad nacionalidad, string dato)
+		{
+			int retornoDniInt = default(int);
+
+			if (int.TryParse(dato, out retornoDniInt))
+				retornoDniInt = ValidarDni(nacionalidad, retornoDniInt);
+			else
+				throw new DniInvalidoException();
+
+			return retornoDniInt;
+		}
+
+		/// <summary>
+		/// Método que valida el nombre y apellido de la persona.
+		/// </summary>
+		/// <param name="dato"></param>
+		/// <returns></returns>
+		private string ValidarNombreApellido(string dato)
+		{
+			Regex expresionRegular = new Regex("^[a-zA-ZÁÉÍÓÚáéíóú]*$");
+			string retornoStr = null;
+
+			if (expresionRegular.IsMatch(dato))
+				retornoStr = dato;
+
+			return retornoStr;
+		}
+		#endregion
+
+		#region Override
+		/// <summary>
+		/// Sobrescritura del método ToString() que expone
+		/// los datos de la Persona.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"NOMBRE COMPLETO: {this.Apellido}, {this.Nombre}");
+			sb.AppendLine($"NACIONALIDAD: {this.Nacionalidad}");
+			return sb.ToString();
+		}
+		#endregion
+
+	}
+}
---
+++ TP3/Clases Abstractas/Universitario.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesAbstractas
+{
+    public abstract class Universitario : Persona
+    {
+        #region Atributos
+        private int legajo;
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor por defecto de Universitario.
+        /// </summary>
+        public Universitario()
+        {
+        }
+
+        /// <summary>
+        /// Constructor que llama la base e inicializa el atributo:
+        /// - legajo.
+        /// </summary>
+        /// <param name="legajo"></param>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        public Universitario(int legajo, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : base(nombre, apellido, dni, nacionalidad)
+        {
+            this.legajo = legajo;
+        }
+        #endregion
+
+        /// <summary>
+        /// Propiedad pura y exclusivamente para la serialización XML
+        /// </summary>
+        public int Legajo
+        {
+            get
+            {
+                return this.legajo;
+            }
+            set
+            {
+                this.legajo = value;
+            }
+        }
+        #region Métodos
+        /// <summary>
+        /// Método que expone los datos del universitario.
+        /// </summary>
+        /// <returns></returns>
+        protected virtual string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"{base.ToString()}\nLEGAJO NÚMERO: {this.legajo}\n\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Método abstracto que tendrá implementación en las 
+        /// clases heredadas.
+        /// </summary>
+        /// <returns></returns>
+        protected abstract string ParticiparEnClase();
+        #endregion
+
+        #region Operaciones
+        /// <summary>
+        /// Sobrecarga del operador "==" que compara dos universitarios.
+        /// </summary>
+        /// <param name="uni1"></param>
+        /// <param name="uni2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Universitario uni1, Universitario uni2)
+        {
+            return uni1.Equals(uni2);
+        }
+
+        /// <summary>
+        /// Sobrecarga del operador "!=" que compara si un universitario es 
+        /// distinto de otro.
+        /// </summary>
+        /// <param name="uni1"></param>
+        /// <param name="uni2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Universitario uni1, Universitario uni2)
+        {
+            return !(uni1 == uni2);
+        }
+        #endregion
+
+        #region Override
+        /// <summary>
+        /// Sobrecarga del método Equals() que verifica
+        /// Si el obj pasado por parámetro es:
+        /// - Universitario, tiene el mismo legajo y el mismo dni.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            bool sonIguales = false;
+            if (obj is Universitario)
+            {
+                if (((Universitario)obj).legajo == this.legajo || ((Universitario)obj).DNI == this.DNI)
+                    sonIguales = true;
+            }
+            return sonIguales;
+        }
+        #endregion
+    }
+}
