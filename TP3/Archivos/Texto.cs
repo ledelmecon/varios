ca524dd0579df5ada3c644e1099f704bbf6bdb52
ledelmecon <leandro.delgado@outlook.com>
2020-11-02 02:10:31 -0300

    Add files via upload

---
+++ TP3/Archivos/Texto.cs
@@ -0,0 +1,79 @@
+﻿using Excepciones;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        #region Implementación métodos interface (IArchivo)
+
+        /// <summary>
+        /// Implementación del método de la interface IArchivos
+        /// que guarda datos en formato de texto.
+        /// Caso contrario, lanzará la excepción: ArchivosException().
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool Guardar(string archivo, string datos)
+        {
+            bool pudoGuardar = false;
+
+            try
+            {
+                if (!(archivo is null))
+                {
+                    using (StreamWriter sw = new StreamWriter(archivo))
+                    {
+                        sw.WriteLine(datos);
+                        pudoGuardar = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArchivosException(ex);
+            }
+
+            return pudoGuardar;
+        }
+
+        /// <summary>
+        /// Implementación del método de la interface IArchivos
+        /// que lee datos en formato de texto.
+        /// Caso contrario, lanzará la excepción: ArchivosException().
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool Leer(string archivo, out string datos)
+        {
+            bool pudoLeer = false;
+            datos = string.Empty;
+
+            try
+            {
+                if (!(archivo is null))
+                {
+                    using (StreamReader sr = new StreamReader(archivo))
+                    {
+                        datos = sr.ReadToEnd();
+                        pudoLeer = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArchivosException(ex);
+            }
+
+            return pudoLeer;
+        }
+        #endregion
+    }
+}
---
+++ TP3/Archivos/Xml.cs
@@ -0,0 +1,94 @@
+﻿using Excepciones;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Archivos
+{
+    public class Xml<T> : IArchivo<T>
+    {
+        #region Implementación métodos interface (IArchivo)
+
+        /// <summary>
+        /// Implementación del método de la interface IArchivos
+        /// que serializa datos en formato XML.
+        /// Caso contrario, lanzará la excepción: ArchivosException().
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool Guardar(string archivo, T datos)
+        {
+            bool pudoSerializar = false;
+            XmlTextWriter xmlWriter;
+            XmlSerializer xmlSer;
+
+            try
+            {
+                if (!(archivo is null))
+                {
+                    if (Path.GetExtension(archivo) is ".xml")
+                    {
+                        using (xmlWriter = new XmlTextWriter(archivo, Encoding.UTF8))
+                        {
+                            xmlWriter.Formatting = Formatting.Indented;
+                            xmlSer = new XmlSerializer(typeof(T));
+                            xmlSer.Serialize(xmlWriter, datos);
+                            pudoSerializar = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArchivosException(ex);
+            }
+
+            return pudoSerializar;
+        }
+
+        /// <summary>
+        /// Implementación del método de la interface IArchivos
+        /// que deserializa datos en formato XML.
+        /// Caso contrario, lanzará la excepción: ArchivosException().
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool Leer(string archivo, out T datos) 
+        {
+            bool pudoDeserializar = false;
+            XmlTextReader xmlReader;
+            XmlSerializer xmlSer;
+            datos = default;
+
+            try
+            {
+                if (!(archivo is null))
+                {
+                    if (Path.GetExtension(archivo) is ".xml")
+                    {
+                        using (xmlReader = new XmlTextReader(archivo))
+                        {
+                            xmlSer = new XmlSerializer(typeof(T));
+                            datos = (T)xmlSer.Deserialize(xmlReader);
+                            pudoDeserializar = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArchivosException(ex);
+            }
+
+            return pudoDeserializar;
+        }
+        #endregion
+    }
+}
